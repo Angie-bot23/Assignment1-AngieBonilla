c1563120ba4cba21caaba42e350b148f84729ee5
Angie-bot23 <angie.bonilla@msj.edu>
2026-04-17 13:07:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const questions = document.querySelectorAll(".faq-question");
+
+questions.forEach((question) => {
+  question.addEventListener("click", () => {
+    const answer = question.nextElementSibling;
+    answer.classList.toggle("show");
+  });
+});
+
+
+const toggleBtn = document.getElementById("toggle-mode");
+const body = document.body;
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("night-mode");
+  toggleBtn.textContent = document.body.classList.contains("night-mode")
+    ? "Switch to Day Mode"
+    : "Switch to Night Mode";
+});
 
